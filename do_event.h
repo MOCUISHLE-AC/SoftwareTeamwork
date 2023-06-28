920733d825b284b5224a5550fd6582365a279a95
linlingw <102218712+linlingw@users.noreply.github.com>
2023-06-28 19:39:23 +0800

    Add files via upload

---
+++ do_event.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+void zero_event();
+
+void c_event(char* x);
+
+void s_event(char* x);
+
+void n_event(char* x);
+
+void n_m_event(char* x, char* y);
+
+void n_r_event(char* x, char* y);
+
+void n_u_event(char* x);
